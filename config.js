276947a31f1e10f8f99db994bfcbe32016659a66
Adam Kreiner <adamkreiner@gmail.com>
2020-12-07 14:55:12 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '714406139196439',
+        clientSecret: 'e335fbb84e98edf963c54674711df49a'
+    }
 }
